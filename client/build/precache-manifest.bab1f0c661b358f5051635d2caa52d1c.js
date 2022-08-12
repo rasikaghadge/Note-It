5430f8f09991b7c108b587e59e8853100e7178db
Rasika Ghadge <90198493+rasikaghadge@users.noreply.github.com>
2022-08-12 14:24:02 +0530

    final

---
+++ client/build/precache-manifest.bab1f0c661b358f5051635d2caa52d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50c02d8084755ff511ea1f34cb24286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d71f2974a08eab2a196",
+    "url": "/static/css/main.6927dd65.chunk.css"
+  },
+  {
+    "revision": "08bfe183196f03331ccd",
+    "url": "/static/js/2.ed1285a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed1285a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d71f2974a08eab2a196",
+    "url": "/static/js/main.a1dcb953.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -30,9 +30,9 @@ mongoose.connect(URI, {
 
 // should be below MongoDB and  Above Listen Sever
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('/*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     });
 }
 
